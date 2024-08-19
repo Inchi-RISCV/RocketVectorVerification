f8fc4104663ea0e0931229df5b79504440acb669
huyiru67 <huyiru@inchitech.com>
2024-08-19 16:13:20 +0800

    add partial mask store cases

--- dcache/tc/sequence/dcache_partial_mask_store_sequence.sv
+++ dcache/tc/sequence/dcache_partial_mask_store_sequence.sv
@@ -36,16 +36,32 @@ class dcache_partial_mask_store_sequence extends dcache_base_sequence;
 		bit [63:0] 	req_wmask;
 		bit [511:0]	wdata, exp_data;
 		bit success;
+		string init_state;
 
 	 	super.body(); 
     `uvm_info(get_type_name(), "dcache sequence starting", UVM_NONE)
 
+		init_state = vmm_opts::get_string("init_state", "N", "init_state");
 
 		//TODO:
 		length 			= 20;
 		tag_idx_t 	= 'h4_0000;
 
 		dcache_random_cfg(addr_t,tag_idx_t,set_idx_t,word_idx_t,bank_idx_t,row_offset_t);
+
+		for(int i=0;i<length;i++)begin
+			req_source 	= $urandom_range(3);
+
+			if(init_state == "B") begin
+				dcache_load(req_source,addr_t+'h40*i);
+			end
+			else if(init_state == "Trunk") begin
+				//TODO
+			end
+			else if(init_state == "Dirty") begin
+				dcache_store(req_source,addr_t+'h40*i,{16{'habababa0}}+i);
+			end
+		end
 
 		for(int i=0;i<length;i++)begin
 			req_source 	= $urandom_range(3);
@@ -57,8 +73,6 @@ class dcache_partial_mask_store_sequence extends dcache_base_sequence;
 					exp_data[8*k+:8] = wdata[8*k+:8];
 				end
 			end
-			`uvm_info("yrhu debug",$sformatf("req_wmask = %0h", req_wmask),UVM_LOW);
-			`uvm_info("yrhu debug",$sformatf("exp_data = %0h", exp_data),UVM_LOW);
 
 			dcache_partial_mask_store(req_source,addr_t+'h40*i,wdata,req_wmask);
 		end
