18a039754e246994975d0564382447b5a3c74b45
huyiru67 <huyiru@inchitech.com>
2024-08-28 17:31:06 +0800

    add load store hazard case

--- dcache/tc/sequence/dcache_hazard_sequence.sv
+++ dcache/tc/sequence/dcache_hazard_sequence.sv
@@ -3,7 +3,7 @@
 // Company           : Inchi Technology .Inc
 //============================================================================
 // Project           :dcache
-// File Name         :dcache_load_hazard_sequence.sv
+// File Name         :dcache_hazard_sequence.sv
 // Author            :huangxiaogang
 // Email             :huangxiaogang@inchitech.com
 // Called by         :
@@ -13,14 +13,14 @@
 // Description       :
 //============================================================================
 
-`ifndef _dcache_load_hazard_sequence_SV_
-`define _dcache_load_hazard_sequence_SV_
+`ifndef _dcache_hazard_sequence_SV_
+`define _dcache_hazard_sequence_SV_
 
-class dcache_load_hazard_sequence extends dcache_base_sequence;
-  `uvm_object_utils(dcache_load_hazard_sequence)
+class dcache_hazard_sequence extends dcache_base_sequence;
+  `uvm_object_utils(dcache_hazard_sequence)
 	`uvm_declare_p_sequencer(dcache_vsqr)
 
-	function new(string name = "dcache_load_hazard_sequence");
+	function new(string name = "dcache_hazard_sequence");
     super.new(name);
   endfunction
 
@@ -32,36 +32,55 @@ class dcache_load_hazard_sequence extends dcache_base_sequence;
 		bit 			word_idx_t;
 		bit [1:0]	bank_idx_t;
 		bit [2:0]	row_offset_t;
-		bit is_mmio_range;
-		bit noAlloc;
 		bit [7:0] req_source;
+		bit [4:0] cmd, cmd1, cmd2;
 
 	 	super.body(); 
     `uvm_info(get_type_name(), "dcache sequence starting", UVM_NONE)
 		
-		is_mmio_range = vmm_opts::get_int("is_mmio_range", 0, "is_mmio_range");
-		noAlloc = vmm_opts::get_int("noAlloc", 0, "noAlloc");
+		cmd1 = vmm_opts::get_int("cmd1", 0, "cmd1");
+		cmd2 = vmm_opts::get_int("cmd2", 0, "cmd2");
 
 		//TODO:
 		length 			= 100;
 		req_source	= $urandom_range(3);
-		if(is_mmio_range) begin
-			tag_idx_t 	= 'h3_0000;
-		end
-		else begin
-			tag_idx_t 	= 'h4_0000;
-		end
+		tag_idx_t 	= 'h4_0000;
+		cmd 	= $urandom_range(1);
 
 		dcache_random_cfg(addr_t,tag_idx_t,set_idx_t,word_idx_t,bank_idx_t,row_offset_t);
 		
-		backdoor_put_data(addr_t,6,{16{'h76543210}});
+		backdoor_put_data(addr_t,6,{16{'haaaa_0000}});
+
+/*
+		if(cmd1 == 0) begin
+			dcache_load(req_source,addr_t);
+		end
+		else if(cmd1 == 1) begin
+			dcache_store(req_source,addr_t,{16{'h76543210}});
+		end
 
 		for(int i=0;i<length;i++)begin
-			dcache_load(req_source,addr_t,,,noAlloc);
+			if(cmd2 == 0) begin
+				dcache_load(req_source,addr_t);
+			end
+			else if(cmd2 == 1) begin
+				dcache_store(req_source,addr_t,{16{'h76543210+i}});
+			end
+		end
+*/
+		
+		for(int i=0;i<length;i++)begin
+			cmd 	= $urandom_range(1);
+			if(cmd == 0) begin
+				dcache_load(req_source,addr_t);
+			end
+			else if(cmd == 1) begin
+				dcache_store(req_source,addr_t,{16{'h76543210+i}});
+			end
 		end
 
   endtask
 
-endclass : dcache_load_hazard_sequence
+endclass : dcache_hazard_sequence
 
 `endif
--- dcache/tc/tc.f
+++ dcache/tc/tc.f
@@ -3,7 +3,7 @@
 ./sequence/dcache_base_sequence.sv
 ./sequence/dcache_sanity_sequence.sv
 ./sequence/dcache_load_sequence.sv
-./sequence/dcache_load_hazard_sequence.sv
+./sequence/dcache_hazard_sequence.sv
 ./sequence/dcache_store_sequence.sv
 ./sequence/dcache_addr_random_sequence.sv
 ./sequence/dcache_release_sequence.sv
@@ -18,7 +18,7 @@
 ./dcache_base_test.sv
 ./dcache_sanity_test.sv
 ./dcache_load_test.sv
-./dcache_load_hazard_test.sv
+./dcache_hazard_test.sv
 ./dcache_store_test.sv
 ./dcache_addr_random_test.sv
 ./dcache_release_test.sv
