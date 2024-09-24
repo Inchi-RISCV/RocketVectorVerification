a059ad1f727019445f35fcd91e24bdba1334499a
huyiru67 <huyiru@inchitech.com>
2024-09-24 11:00:57 +0800

    long delay test override tilelink configuration

--- dcache/env/tilelink_agent/new_tilelink_system_configuration.sv
+++ dcache/env/tilelink_agent/new_tilelink_system_configuration.sv
@@ -1,38 +1,26 @@
 
+`ifndef NEW_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
+`define NEW_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
 
-`ifndef GUARD_CUST_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
-`define GUARD_CUST_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
-
-//-----------------------------------------------------------------------------
-/**
- * Abstract:
- * Class cust_svt_tilelink_system_configuration is basically used to encapsulate all
- * the configuration information.  It extends system configuration and set the
- * appropriate fields like number of master/slaves, create master/slave
- * configurations etc..., which are required by System Env.
- */
 `include "svt_tilelink.uvm.pkg"
 import svt_tilelink_uvm_pkg::*;
 
-class cust_svt_tilelink_system_configuration extends svt_tilelink_system_configuration;
+class new_tilelink_system_configuration extends cust_svt_tilelink_system_configuration;
 
-  // Utility macro
-  `svt_xvm_object_utils (cust_svt_tilelink_system_configuration)
+  `svt_xvm_object_utils (new_tilelink_system_configuration)
 
-  //---------------------------------------------------------------------------
-  /** Class construct. */
-  function new (string name="cust_svt_tilelink_system_configuration");
+  function new (string name="new_tilelink_system_configuration");
     super.new(name);
 	endfunction 
 
 	extern virtual function void set_tilelink_cfg();
 
-endclass : cust_svt_tilelink_system_configuration
+endclass : new_tilelink_system_configuration
 
-function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
+function void new_tilelink_system_configuration::set_tilelink_cfg();
     int selector;
-    //super.new(str);
-    num_slave = 1;
+    
+		num_slave = 1;
     num_master = 1;
     create_sub_cfgs(num_master, num_slave);
 
@@ -42,19 +30,14 @@ function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
       this.master_cfg[i].num_outstanding_txn = $urandom_range(10,100);
 		  this.master_cfg[i].addr_width = 39;
       this.master_cfg[i].data_width = 512;			
-      //this.master_cfg[i].min_d_rdy_d_rdy_assert_delay = $urandom_range(0,3);
-      //this.master_cfg[i].max_d_rdy_d_rdy_assert_delay = $urandom_range(this.master_cfg[i].min_d_rdy_d_rdy_assert_delay,7);
       this.master_cfg[i].min_d_rdy_deassert_delay = $urandom_range(0,3);
       this.master_cfg[i].max_d_rdy_deassert_delay = $urandom_range(this.master_cfg[i].min_d_rdy_deassert_delay,7);
-      //this.master_cfg[i].min_d_vld_d_rdy_assert_delay = $urandom_range(0,3);
-      //this.master_cfg[i].max_d_vld_d_rdy_assert_delay = $urandom_range(this.master_cfg[i].min_d_vld_d_rdy_assert_delay,7);
     end
 
     foreach(this.slave_cfg[i]) begin
 			this.slave_cfg[i].is_active = 1;
 			this.slave_cfg[i].slv_delay_en = 1;
 			this.slave_cfg[i].slv_vld_rdy_delay_en = 0;
-			//this.slave_cfg[i].slv_vld_rdy_delay_en = 1;
 			this.slave_cfg[i].slv_cross_chnl_delay_en = 1;
       this.slave_cfg[i].addr_width = 39;
       this.slave_cfg[i].data_width = 512;   
@@ -65,8 +48,8 @@ function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
       this.slave_cfg[i].max_a_rdy_a_rdy_assert_delay = 0;
 			this.slave_cfg[i].min_a_rdy_deassert_delay = 0;
       this.slave_cfg[i].max_a_rdy_deassert_delay = 0;
-      this.slave_cfg[i].min_a_vld_d_vld_cross_chnl_delay = 60;
-      this.slave_cfg[i].max_a_vld_d_vld_cross_chnl_delay = 200;
+      this.slave_cfg[i].min_a_vld_d_vld_cross_chnl_delay = 1000; 	//TODO: to be confirmed
+      this.slave_cfg[i].max_a_vld_d_vld_cross_chnl_delay = 2000; 	//TODO: to be confirmed
 			this.slave_cfg[i].min_d_vld_d_vld_assert_delay = 0;
       this.slave_cfg[i].max_d_vld_d_vld_assert_delay = 0;
       
@@ -76,11 +59,9 @@ function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
       this.slave_cfg[i].enable_cov= 1;
 		  this.slave_cfg[i].enable_xml_gen= 1;
 		  this.slave_cfg[i].enable_pa_writer= 1;
-			//this.slave_cfg[i].pa_format_type= 2;
 	    this.slave_cfg[i].same_cycle_resp_en= 0;
-			//this.slave_cfg[i].enable_reporting=1;
     end
 endfunction
 
-`endif // GUARD_CUST_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
+`endif // NEW_SVT_TILELINK_SYSTEM_CONFIGURATION_SV
 
---
+++ dcache/tc/sequence/dcache_long_delay_sequence.sv
@@ -0,0 +1,60 @@
+
+`ifndef _dcache_long_delay_sequence_SV_
+`define _dcache_long_delay_sequence_SV_
+
+class dcache_long_delay_sequence extends dcache_base_sequence;
+  `uvm_object_utils(dcache_long_delay_sequence)
+	`uvm_declare_p_sequencer(dcache_vsqr)
+
+	function new(string name = "dcache_long_delay_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+		bit [31:0] length;
+		bit [38:0] addr_t;
+		bit [26:0] tag_idx_t;
+		bit [6:0] set_idx_t;
+		bit [511:0] wdata;
+		bit success;
+		bit is_mmio_range;
+		bit noAlloc;
+
+	 	super.body(); 
+    `uvm_info(get_type_name(), "dcache sequence starting", UVM_NONE)
+
+		is_mmio_range = vmm_opts::get_int("is_mmio_range", 0, "is_mmio_range");
+		noAlloc = vmm_opts::get_int("noAlloc", 0, "noAlloc");
+
+		//TODO:
+		length 			= $urandom_range(100, 500);
+		`uvm_info("RANDOM_CFG",$sformatf("length = %0d", length),UVM_LOW);
+
+		if(is_mmio_range) begin
+			success 	= std::randomize(tag_idx_t,set_idx_t) with {
+				tag_idx_t inside {['h3_0000:'h3_ffff]};
+				set_idx_t inside {[0:127]};
+				((tag_idx_t<<13)+(set_idx_t<<6)+64*length) inside {['h6000_0000:'h7fff_ffff]};
+			};
+		end
+		else begin
+			success 	= std::randomize(tag_idx_t,set_idx_t) with {
+				tag_idx_t inside {['h4_0000:'h7_ffff]};
+				set_idx_t inside {[0:127]};
+				((tag_idx_t<<13)+(set_idx_t<<6)+64*length) inside {['h8000_0000:'hffff_ffff]};
+			};
+		end
+
+		dcache_random_cfg(addr_t,tag_idx_t,set_idx_t);
+		
+		for(int i=0;i<length;i++)begin
+			backdoor_put_data(addr_t+'h40*i,6,{16{'h76543210}}+i);
+	  end
+
+		for(int i=0;i<length;i++)begin
+			dcache_load(addr_t+'h40*i,,,noAlloc);
+		end
+  endtask
+endclass : dcache_long_delay_sequence
+
+`endif
