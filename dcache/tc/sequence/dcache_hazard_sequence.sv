ea3226785af5db3374db0a86bce3e2873f181efe
huyiru67 <huyiru@inchitech.com>
2024-09-29 14:19:55 +0800

    add hazard cases & regression script

--- dcache/tc/sequence/dcache_hazard_sequence.sv
+++ dcache/tc/sequence/dcache_hazard_sequence.sv
@@ -16,10 +16,13 @@ class dcache_hazard_sequence extends dcache_base_sequence;
 		bit [26:0] tag_idx_t;
 		bit [6:0] set_idx_t;
 		bit [511:0] wdata;
+		bit [63:0]  wmask;
+		bit [2:0]	size_t;
 		bit [4:0] cmd, cmd1, cmd2;
 		string resp_status;
 		string init_state;
 		bit random_hazard;
+		bit write_cmd_random;
 		bit success;
 
 	 	super.body(); 
@@ -34,7 +37,7 @@ class dcache_hazard_sequence extends dcache_base_sequence;
 		if(resp_status == "hit") begin
 			length1 			= 50;
 		end
-		else if(resp_status == "miss") begin
+		else begin 	//miss or lr
 			length1 			= 1;
 		end
 		length2 			= $urandom_range(50, 500);
@@ -47,19 +50,26 @@ class dcache_hazard_sequence extends dcache_base_sequence;
 
 		dcache_random_cfg(addr_t,tag_idx_t,set_idx_t);
 		
-
-		cmd 	= $urandom_range(1);
-		success	= std::randomize(wdata) with {wdata <= (2**512-1);};
 		backdoor_put_data(addr_t,6,{16{'haaaa_0000}});
 		
+		write_cmd_random 	= $urandom_range(1);
+
 		if(random_hazard) begin
 			for(int i=0;i<length2;i++)begin
 				cmd 	= $urandom_range(1);
+				success = std::randomize(wdata) with { wdata <= (2**512-1);};
+				success = std::randomize (wmask) with {wmask <= (2**(2**6))-1;};
+				
 				if(cmd == 0) begin
 					dcache_load(addr_t);
 				end
 				else if(cmd == 1) begin
-					dcache_store(addr_t,wdata);
+					if(!write_cmd_random) begin
+						dcache_store(addr_t,wdata);
+					end
+					else begin
+						dcache_partial_mask_store(addr_t,wdata,wmask,1);
+					end
 				end
 			end
 		end 
@@ -72,8 +82,16 @@ class dcache_hazard_sequence extends dcache_base_sequence;
 				if(cmd1 == 0) begin 	//LOAD
 					dcache_load(addr_t);
 				end
-				else if(cmd1 == 1) begin	//STORE
-					dcache_store(addr_t,wdata);
+				else if(cmd1 == 1) begin	//STORE/Partial_Masked_Store
+					success = std::randomize(wdata) with { wdata <= (2**512-1);};
+					success = std::randomize (wmask) with {wmask <= (2**(2**6))-1;};
+					
+					if(!write_cmd_random) begin
+						dcache_store(addr_t,wdata);
+					end
+					else begin
+						dcache_partial_mask_store(addr_t,wdata,wmask);
+					end
 				end
 				else if(cmd1 == 2) begin	//PFR
 					dcache_prefetch_read(addr_t);
@@ -81,20 +99,42 @@ class dcache_hazard_sequence extends dcache_base_sequence;
 				else if(cmd1 == 3) begin	//PFW
 					dcache_prefetch_write(addr_t);
 				end
+				else if(cmd1 == 4) begin	//LR
+					size_t = $urandom_range(2,3);
+					dcache_lr(addr_t,size_t);
+				end
 			end
 
 			for(int i=0;i<length2;i++)begin
 				if(cmd2 == 0) begin 	//LOAD
 					dcache_load(addr_t);
 				end
-				else if(cmd2 == 1) begin	//STORE
-					dcache_store(addr_t,wdata);
+				else if(cmd2 == 1) begin	//STORE/Partial_Masked_Store
+					success = std::randomize(wdata) with { wdata <= (2**512-1);};
+					success = std::randomize (wmask) with {wmask <= (2**(2**6))-1;};
+					
+					if(!write_cmd_random) begin
+						dcache_store(addr_t,wdata);
+					end
+					else begin
+						dcache_partial_mask_store(addr_t,wdata,wmask);
+					end
 				end
 				else if(cmd2 == 2) begin	//PFR
 					dcache_prefetch_read(addr_t);
 				end
 				else if(cmd2 == 3) begin	//PFW
 					dcache_prefetch_write(addr_t);
+				end
+				else if(cmd2 == 4) begin	//LR
+					size_t = $urandom_range(2,3);
+					dcache_lr(addr_t,size_t);
+				end
+				else if(cmd2 == 5) begin	//SC
+					size_t = $urandom_range(2,3);
+					success = std::randomize(wdata) with { wdata <= (2**512-1);};
+
+					dcache_sc(addr_t,wdata,size_t);
 				end
 			end
 		end	
