0b1a8bd9d3751a4322bcedb0c36b3f2326771136
huyiru67 <huyiru@inchitech.com>
2024-12-03 11:15:07 +0800

    change VIP configuration for slave mem_address_range

--- dcache/env/tilelink_agent/cust_svt_tilelink_system_configuration.sv
+++ dcache/env/tilelink_agent/cust_svt_tilelink_system_configuration.sv
@@ -73,7 +73,7 @@ function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
       this.slave_cfg[i].min_c_vld_c_rdy_assert_delay = 0;
       this.slave_cfg[i].max_c_vld_c_rdy_assert_delay = 3;
 			this.slave_cfg[i].min_e_vld_e_rdy_assert_delay = 0;
-      this.slave_cfg[i].max_e_vld_e_rdy_assert_delay = 0; 	//TODO: vip monitor delay
+      this.slave_cfg[i].max_e_vld_e_rdy_assert_delay = 0; 	//TODO: vip not support, monitor delay
 			this.slave_cfg[i].enable_tracing = 1;
       this.slave_cfg[i].enable_chk_fail_cov= 1;
       this.slave_cfg[i].enable_chk_pass_cov= 1;
@@ -82,6 +82,7 @@ function void cust_svt_tilelink_system_configuration::set_tilelink_cfg();
 		  this.slave_cfg[i].enable_pa_writer= 1;
 			//this.slave_cfg[i].pa_format_type= 2;
 	    this.slave_cfg[i].same_cycle_resp_en= 0;
+	    this.slave_cfg[i].mem_address_range = 'h7f_ffff_ffff;
 			//this.slave_cfg[i].enable_reporting=1;
     end
 endfunction
